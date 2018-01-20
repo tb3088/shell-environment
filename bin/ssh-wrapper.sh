0125b4bf3d7d5d68e89c9012098343eddfea9036
Matthew Patton <matthew.patton@itgfirm.com>
2018-01-20 02:14:34 -0500

    reorganize and simplify

--- bin/ssh-wrapper.sh
+++ bin/ssh-wrapper.sh
@@ -12,100 +12,110 @@ shopt -s nullglob
 
 case `uname -o` in
   Cygwin*) 
-	WHICH='\which --skip-functions --skip-alias'
+      WHICH='\which --skip-functions --skip-alias'
 	;;
 esac
+
 : ${WHICH:=which}
-: ${SSH:=`$WHICH ssh 2>/dev/null`}
-: ${SCP:=`$WHICH scp 2>/dev/null`}
-: ${SFTP:=`$WHICH sftp 2>/dev/null`}
-: ${SCREEN:=`$WHICH screen 2>/dev/null`}
-
-[ -x "$SSH" -a -x "$SCP" -a -x "$SFTP" ] || { 
-    >&2 echo -e " missing binaries!\n  SSH=$SSH\n  SCP=$SCP\n  SFTP=$SFTP\n"
-    exit 2;
-  }
-
-# compute from wrapper filename
-: ${PROFILE:=`basename -s .sh "$0"`}
-
-# disable Screen
-for s in ${NO_SCREEN:-git rsync}; do
-    [ "${PROFILE##*.}" = "$s" ] && { unset SCREEN; break; }
+for p in SSH SCP SFTP SCREEN; do
+  [ "${!p:0:1}" = '/' ] || eval $p=`$WHICH ${!p:-${p,,}} 2>/dev/null`
+  [ -x "${!p}" ] || {
+      >&2 echo -e "ERROR: missing $p binary '${!p}'"; exit 2; }
 done
 
 function runv() {
-    echo >&2 "+ $*"
-    "$@"
+  >&2 echo "+ $*"
+  "$@"
 }
 
 function _ssh() {
   # environment:
   #   SCREEN   - if set but empty, disable use of screen
-  #   PROFILE  - name of SSH configuration file (-F)
+  #   SSH_CONFIG - name of SSH configuration file (-F)
+  #   PROFILE  - stub used to compute SSH_CONFIG
   #   IDENTITY - path to identity file (-i)
 
   shopt -s extglob
-  local _screen _conf _ident _cmd
+  local _screen _conf _ident _cmd _env
+  local i v p
 
   if [ "${TERM%.*}" = "screen" ]; then
-      _screen="$SCREEN";
-      TERM=${TERM/screen./}
-      TERM=${TERM/screen/}
-#    : ${TERM:=linux}
+    _screen="$SCREEN";
+    TERM=${TERM/screen./}
+    TERM=${TERM/screen/}
   fi
-
-  [ -n "$IDENTITY" -a ! -f "$IDENTITY" ] && {
-      >&2 echo "ERROR: '$IDENTITY' not found"; return 1
-  }
-  [ -n "$SSH_CONFIG" -a ! -f "$SSH_CONFIG" ] && {
-      >&2 echo "ERROR: '$SSH_CONFIG' not found"; return 1
-  }
-
-  if [ -z "$SSH_CONFIG" ]; then
-      # attempt a quick search - traversing CWD not advised.
-      for _conf in $HOME/{.ssh,.aws/$AWS_PROFILE}/{,$PROFILE{,/$AWS_REGION}/}config{,{.,-,_}$PROFILE} ; do
-          [ -f "$_conf" ] && {
-	      SSH_CONFIG="$_conf"
-	      >&2 echo "+ found '$SSH_CONFIG' for SSH_CONFIG"
-	      break
-	  }
-      done
-      [ -n "$SSH_CONFIG" ] || { >&2 echo "ERROR: search for SSH_CONFIG ($PROFILE) failed"; return 1; }
-  fi
-# casting about for IDENTITY is dangerous, don't do it!
-#	{,${HOME:-\~}/{,.ssh,.aws/$AWS_PROFILE}/}{"$IDENTITY",id_rsa,$PROFILE}{,.pem}
-
-  # UserKnownHostFile shouldn't be hard-coded inside 'config' because that makes it non-portable
-  SSH_OPTS+=" -o UserKnownHostsFile=${SSH_CONFIG%/*}/known_hosts"
 
   _cmd=SSH
   case ${1^^} in
     SCP|SFTP)
-	_cmd=${1^^}
-	;&
+        _cmd=${1^^}
+        ;&
     SSH)
-	shift; 
-      # disable Screen where persistent command output is helpful
-	unset _screen
-	;;
+        shift; 
+        # disable Screen where persistent command output is helpful
+        unset _screen
+        ;;
   esac
 
-  : ${DEBUG:+${VERBOSE:=-v}}
-  _env=
-  for v in SSH_CONFIG SSH_OPTS PROFILE IDENTITY ${!AWS_*} VERBOSE; do
-      [ -n "${!v}" ] || continue
-      _env+=" $v='${!v}'"
+  for i in `seq ${DEBUG:-0} 2>/dev/null`; do
+    VERBOSE+=' -v'
   done
 
-#${DEBUG:+runv} ${_screen:+$_screen -t "$PROFILE" ${TERM:+-T $TERM} bash -c \"$_vars} ${!_cmd} ${VERBOSE:+-v} \
+  for v in IDENTITY SSH_CONFIG; do
+    [ -n "${!v}" -a ! -f "${!v}" ] && {
+        >&2 echo "ERROR: $v file '${!v}' not found"; return 1; }
+  done
+
+  if [ -z "$SSH_CONFIG" ]; then
+    # attempt a quick search - traversing CWD not advised.
+    for _conf in $HOME/{.ssh,.aws/$AWS_PROFILE}/{,$PROFILE{,/$AWS_REGION}/}config{,{.,-,_}$PROFILE} ; do
+      [ -f "$_conf" ] && {
+	        SSH_CONFIG="$_conf"
+	        >&2 echo "INFO: found SSH_CONFIG '$SSH_CONFIG' ${PROFILE+for PROFILE '$PROFILE'}"
+	        break; }
+    done
+    [ -n "$SSH_CONFIG" ] || { 
+        >&2 echo "ERROR: no SSH_CONFIG found ${PROFILE+for PROFILE '$PROFILE'}"; return 1; }
+  fi
+
+  # UserKnownHostFile shouldn't be hard-coded inside 'config' because brittle
+  : ${SSH_KNOWN_HOSTS:="${SSH_CONFIG%/*}/known_hosts"}
+
+# casting about for IDENTITY is risky, don't do it!
+#	{,${HOME:-\~}/{,.ssh,.aws/$AWS_PROFILE}/}{"$IDENTITY",id_rsa,$PROFILE}{,.pem}
+
+  # propagate environment when running Screen
+  _env=
+  if [ -n "${_screen}" ]; then
+    for v in SSH_CONFIG PROFILE IDENTITY ${!AWS_*} VERBOSE; do
+      [ -n "${!v}" ] || continue
+      _env+=" $v='${!v}'"
+    done
+  fi
+
   ${DEBUG:+runv} eval ${_screen:+ $_screen -t "$PROFILE:$1" ${TERM:+ -T $TERM} bash -c \"${_env:+ env $_env}} \
-	${!_cmd} ${VERBOSE:- -q} \
-	${IDENTITY:+ -i "$IDENTITY"} \
-	${SSH_CONFIG:+ -F "$SSH_CONFIG"} \
-	$SSH_OPTS \
-	"$@" ${_screen:+ ${DEBUG:+|| sleep 5}\"}
-#	${DEBUG:+ 2>"/tmp/ssh-$PROFILE_$1.err"}
+      ${!_cmd} $VERBOSE \
+      ${IDENTITY:+ -i "$IDENTITY"} \
+      ${SSH_CONFIG:+ -F "$SSH_CONFIG"} \
+      ${SSH_KNOWN_HOSTS:+ -o UserKnownHostsFile="$SSH_KNOWN_HOSTS"} \
+      $SSH_OPTS \
+      "$@" ${_screen:+ ${DEBUG:+ || sleep 15}\"}
+
+      #	${DEBUG:+ 2>"/tmp/ssh-$PROFILE_$1.err"}
 }
+
+
+#--- main---
+if [ -z "$PROFILE" ]; then
+  # compute from wrapper filename
+  _origin=$( basename -s .sh `readlink "$0"`)
+  _prog=$( basename -s .sh "$0")
+  [ "$_prog" != "$_origin" ] && PROFILE="$_prog"
+fi
+
+# disable Screen
+for s in ${NO_SCREEN:-git rsync}; do
+  [ "${PROFILE##*.}" = "$s" ] && { unset SCREEN; break; }
+done
 
 _ssh "$@"
