89c0e19fcc6db61a1249c8c46eb39846709392ef
Matthew Patton <mpatton@enquizit.com>
2018-08-21 11:59:21 -0400

    heavy rewrite which can be invoked with Eval or Source `cmd`

--- bin/switch-role.sh
+++ bin/switch-role.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Usage:
+
+function runv() { 
+  echo >&2 "+ $*"
+  "$@"
+}
+
+function cleanup() {
+  echo "unset ${!AWS_*}"
+  exit 1
+}
+
+trap cleanup SIGHUP SIGTERM SIGINT ERR
 
 declare -A fields=(
     [AccessKeyId]=aws_access_key_id 
@@ -7,56 +20,58 @@ declare -A fields=(
     [SessionToken]=aws_session_token
     [Expiration]=aws_session_expire
 )
+cmd_id='get-session'
 
-target="${AWS_PROFILE:?}.${1:?}"
-[ "$AWS_PROFILE" != "$target" ] || return 0
+profile="${1:-${AWS_PROFILE:?}}"
 
-case $target in
-  *.reset|*.clear)
-	unset ${!AWS_*}
-	AWS_PROFILE=${target%%.*}
-	return 0
-	;;
-  nyu.*) 
+case "$profile" in
+  nyu|nyu.*) 
 	account_id=120017232434
 	role=
 	mfa="arn:aws:iam::$account_id:mfa/patton"
 	;;&
   nyu.it-sandbox) 
 	account_id=231328997772
-	role="OrganizationAccountAccessRole"
+	role='OrganizationAccountAccessRole'
+	cmd_id='assume-role'
 	;;
   nyu.it-dev) 
 	account_id=508939681246
-	role="Migration_Contractor"
+	role='Migration_Contractor'
+	cmd_id='assume-role'
 	;;
-  *)	echo "Error: unsupported value ($target)"; return 1
+  *)	echo "Error: unsupported value ($profile)"; exit 1
 esac
 
-declare -a cmd=(
-    "aws sts assume-role --role-arn arn:aws:iam::${account_id:?}:role/${role:?} --role-session-name rs-$target-$$"
-    "aws sts get-session-token"
+declare -A cmd=(
+    [assume-role]="assume-role --role-arn arn:aws:iam::${account_id:?}:role/${role:?} --role-session-name RSN-$profile-$$"
+    [get-session]='get-session-token'
 )
-    
 
-[ -n "$mfa" -a \( -n "$AWS_SESSION_TOKEN"  -o -n "`aws configure get aws_session_token --profile $AWS_PROFILE`" \) ] || {
 
-  for c in "${cmd[@]}"; do
-    output=`$c ${mfa:+ --serial-number $mfa --token-code ${2:?}} ${DURATION:+ --duration-seconds $DURATION}` && break
-  done
-  [ -n "$output" ] || return 1
+[ "${profile%.*}" == "${AWS_PROFILE%.*}" ] || unset ${!AWS_*}
+AWS_PROFILE=$profile
+unset profile
+
+if [ -n "$mfa" -o \( -z "$AWS_SESSION_TOKEN"  -o \
+	-z "`aws configure get aws_session_token --profile $AWS_PROFILE`" \) -o \
+    false ]; then
+  # TODO replace 'false' with check for expired
+
+  set -euo pipefail
+  output=`${DEBUG:+ runv} aws sts ${cmd[$cmd_id]} ${mfa:+ --serial-number $mfa --token-code ${2:?}} \
+	${DURATION:+ --duration-seconds $DURATION} --profile ${AWS_PROFILE%.*}`
 
   for key in "${!fields[@]}"; do
     val="${fields[$key]}"
     [ -n "$val" ] || continue
 
-    eval ${val^^}=`jq -r .Credentials.$key <<< "$output"`
-#    aws configure set --profile=$target $val `jq -r .Credentials.$key <<< "$output"`
+#    eval ${val^^}=`jq -r .Credentials.$key <<< "$output"`
+    ${DEBUG:+ runv} aws configure set $val `jq -r .Credentials.$key <<< "$output"` --profile $AWS_PROFILE 
   done
-}
+  set +x
+fi
 
-[ -z "$DEBUG" ] || for v in ${!AWS_*}; do echo "$v=${!v}"; done
-
-AWS_PROFILE=$target
-export ${!AWS_*}
+#for v in ${!AWS_*}; do echo "$v='${!v}'"; done
+echo "export AWS_PROFILE=$AWS_PROFILE"
 
