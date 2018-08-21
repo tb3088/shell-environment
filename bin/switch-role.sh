3a031646b53b9767b3828a579e7a4826ecef1263
Matthew Patton <mpatton@enquizit.com>
2018-08-21 06:03:22 -0400

    try assume-role then session

--- bin/switch-role.sh
+++ bin/switch-role.sh
@@ -9,9 +9,14 @@ declare -A fields=(
 )
 
 target="${AWS_PROFILE:?}.${1:?}"
-[ "$AWS_PROFILE" != "$target" ] || exit 2
+[ "$AWS_PROFILE" != "$target" ] || return 0
 
 case $target in
+  *.reset|*.clear)
+	unset ${!AWS_*}
+	AWS_PROFILE=${target%%.*}
+	return 0
+	;;
   nyu.*) 
 	account_id=120017232434
 	role=
@@ -25,14 +30,21 @@ case $target in
 	account_id=508939681246
 	role="Migration_Contractor"
 	;;
-  *)	echo "Error: unsupported value ($1)"; exit 1
+  *)	echo "Error: unsupported value ($target)"; return 1
 esac
 
-
-set -e -o pipefail
+declare -a cmd=(
+    "aws sts assume-role --role-arn arn:aws:iam::${account_id:?}:role/${role:?} --role-session-name rs-$target-$$"
+    "aws sts get-session-token"
+)
+    
 
 [ -n "$mfa" -a \( -n "$AWS_SESSION_TOKEN"  -o -n "`aws configure get aws_session_token --profile $AWS_PROFILE`" \) ] || {
-  output=`aws sts get-session-token ${mfa:+ --serial-number $mfa --token-code $2} ${DURATION:+ --duration-seconds $DURATION}`
+
+  for c in "${cmd[@]}"; do
+    output=`$c ${mfa:+ --serial-number $mfa --token-code ${2:?}} ${DURATION:+ --duration-seconds $DURATION}` && break
+  done
+  [ -n "$output" ] || return 1
 
   for key in "${!fields[@]}"; do
     val="${fields[$key]}"
@@ -43,9 +55,8 @@ set -e -o pipefail
   done
 }
 
-[ -n "$DEBUG" ] && for v in ${!AWS_*}; do echo "$v=${!v}"; done
+[ -z "$DEBUG" ] || for v in ${!AWS_*}; do echo "$v=${!v}"; done
 
-#aws sts assume-role --role-arn "arn:aws:iam::${account_id:?}:assumed-role/${role:?}" --role-session-name "rs-$target-$$" ||
-#aws sts assume-role --role-arn "arn:aws:iam::${account_id:?}:role/${role:?}" --role-session-name "rs-$target-$$"
-#
-#AWS_PROFILE=$target
+AWS_PROFILE=$target
+export ${!AWS_*}
+
