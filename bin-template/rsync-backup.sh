1f74b5ed352064c9b5f0e335d4b888dcf03e3703
Matthew Patton <matthew.patton@itgfirm.com>
2018-10-02 13:44:57 -0400

    use tokens and intermediate variables

--- bin-template/rsync-backup.sh
+++ bin-template/rsync-backup.sh
@@ -1,17 +1,29 @@
-#!/bin/sh
+#!/bin/bash
+
+# Usage: $0 (remote)source (local)dest
+#
+# NOTE - this script ONLY works properly if DEST is local!
+# if remote, the hardlinks, mkdir, and PREVIOUS won't work.
+# 
+# derived from http://www.sanitarium.net/golug/rsync_backups_2010.html
+
+_token='.in-flight'
 
 : ${SOURCE:=/export/home}
-: ${TARGET:=/backup/$SOURCE/`date --iso-8601=minutes`}
-: ${PREVIOUS:=`ls -pt "${TARGET%/*}" | grep '/$' | head -n 1`}
+: ${DEST:=/backup/$SOURCE/`date --iso-8601=minutes`}
+[ -d "$DEST" ] && { >&2 echo "ERROR directory exists ($DEST)"; exit 2; }
 
-mkdir -p "$TARGET"
+: ${PREVIOUS:=$( ls -pt `dirname "$DEST"` | grep '/$' | grep -v "$_token" | head -n 1 )}
 
-rsync --archive --one-file-system --hard-links \
-  ${DEBUG:+ --verbose} ${VERBOSE:+ --progress --itemize-changes} \
+_dest="${DEST%/}$_token"
+mkdir -p "$_dest" || { >&2 echo "ERROR creating in-flight directory ($_dest)"; exit 1; }
+
+rsync ${DEBUG:+ --verbose} ${VERBOSE:+ --progress --itemize-changes} \
+  --archive --one-file-system --hard-links \
   --human-readable --inplace --numeric-ids --delete \
+  --sparse \
   --delete-excluded --exclude-from="$SOURCE/.rsync-exclude" \
-  --link-dest="$PREVIOUS" \
-  ${SOURCE}/ ${TARGET}/
+  ${PREVIOUS:+ --link-dest="$PREVIOUS"} \
+  "${SOURCE%/}/" "$_dest" && mv -vn "$_dest" "${DEST%/}"
 
-# --sparse --whole-file when moving VMs
-# http://www.sanitarium.net/golug/rsync_backups_2010.html
+# --whole-file might be useful on a LAN
