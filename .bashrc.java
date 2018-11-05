1f2ad7348aa6851b742d277fa70e11403986ef77
Matthew Patton <mpatton@enquizit.com>
2018-11-05 17:45:08 -0500

    use variable directly - not dereference

--- .bashrc.java
+++ .bashrc.java
@@ -1,7 +1,7 @@
 which java &>/dev/null && return
 
 for v in "$JAVA_HOME" "${PROGRAMFILES:-/opt}/java${JAVA_VERSION}"; do
-    [ -d "${!v}/bin" ] && addPath "${!v}/bin"
+    [ -d "$v/bin" ] && addPath "$v/bin"
 done
 unset v
 
