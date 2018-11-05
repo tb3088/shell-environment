75bc7303499733b3451a37923fbc1ed7acf57f3b
Matthew Patton <mpatton@enquizit.com>
2018-11-05 17:41:34 -0500

    fix RC=1 and other nits

--- .bashrc.java
+++ .bashrc.java
@@ -1,10 +1,9 @@
 which java &>/dev/null && return
 
-: ${JAVA_HOME:="${PROGRAMFILES:-/opt}/java${JAVA_VERSION}"}
-
-for v in JAVA_HOME; do
-    [ -n "${!v}" ] || { unset $v; continue; }
+for v in JAVA_HOME "${PROGRAMFILES:-/opt}/java${JAVA_VERSION}"; do
+    [ -n "${!v}" ] || continue
     [ -d "${!v}/bin" ] && addPath "${!v}/bin"
 done
+unset v
 
 # vim: set expandtab:ts=4:sw=4
