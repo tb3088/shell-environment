d4a17013ccd23272ce2b1be1459393ecd175db9c
Matthew Patton <mp1116@commscope.com>
2020-03-05 12:42:24 -0500

    make ssh-simple a little smarter

--- bin-template/ssh-simple.sh
+++ bin-template/ssh-simple.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-: ${SSH_CONFIG:="$HOME"/.ssh/`basename $BASH_SOURCE .sh`.config}
+: ${REGION:=${AWS_DEFAULT_REGION:-us-east-1}}
+
+config=`readlink -e $BASH_SOURCE`
+config=`dirname "$config"`/`basename "$config" .sh`/config
+[ -f "$config" ] || config="$HOME/.ssh/`basename $BASH_SOURCE .sh`.config"
+[ -f "$config" ] || unset config
+
+: ${SSH_CONFIG:=${config:-"$HOME/.ssh/config"}}
 : ${SSH_KNOWN_HOSTS:=${SSH_CONFIG/config/known_hosts}}
 
-: ${REGION:=${AWS_DEFAULT_REGION:-us-east-1}}
 
 declare -F runv >/dev/null ||
 function runv() {
     >&2 printf '%s %s\n' `printf '%.0s+' {1..$SHLVL}` "$*"
     [ -n "$NOOP" ] || "$@"
 }
+
+export ${!SSH_*} REGION
 
 ${DEBUG:+ runv} ${SSH:-ssh} ${DEBUG:+ -v} ${SSH_CONFIG:+ -F "$SSH_CONFIG"} ${SSH_KNOWN_HOSTS:+ -o UserKnownHostsFile="$SSH_KNOWN_HOSTS"} "$@"
 
