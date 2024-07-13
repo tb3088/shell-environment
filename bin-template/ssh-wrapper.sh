3f7875cedcb20585fb6a71cb5ac4b858706a4223
Matthew Patton <matthew.patton@tscti.com>
2024-07-12 20:54:36 -0400

    overdue cleanup

--- bin-template/ssh-wrapper.sh
+++ bin-template/ssh-wrapper.sh
@@ -2,15 +2,16 @@
 
 # Usage:
 #
-#   [DEBUG=<0-9>] [VERBOSE=<0-9>|-v ...] [PROFILE=<profile>] [REGION=<region>]
-#       [SSH_IDENTITY=<key>] [SSH_CONFIG=<path>]
-#       ssh-wrapper.sh [options] [cmd] <host> [args]
+#   env [DEBUG=<0-9>] [VERBOSE=<0-9>|-v ...] [PROFILE=<profile>] [REGION=<region>]
+#     [SSH_IDENTITY=<key>] [SSH_CONFIG=<path>]
+#   ssh-wrapper.sh [options] <ssh|scp|sftp> <host> -- [ssh_args]
 #
 # symlink to this wrapper will automatically set PROFILE
 
 source ~/.functions
 is_function log runv || { >&2 echo -e "ERROR\tmissing essential functions (log, runv)"; exit 1; }
 
+shopt -s extglob
 
 #TODO? use recursion
 function genlist() {
@@ -24,49 +25,50 @@ function genlist() {
   # config_REGION                   config_PROFILE
   # [.ssh/]config
 
-  declare -a delim=('/' '.' '_' '-')    # flavor to taste
+  declare -a delims=( '/' '.' '_' '-' )    # flavor to taste
+  # bulk-set D* variable from defaults
+  for i in D{1..3}; do [ -n "${!i:+X}" ] || declare -n $i=delims ; done
 
   local prefix stub combo=()
-  local b c d{1..3} e
-#  declare -I D{1..3}            # inherit from parent
   local item1=$1 item2=$2
   local file
-  : ${file:?}
+  : "${file:?}"
 
-  # bulk-set D* variable from defaults
-  for i in D{1..3}; do declare -p $i &>/dev/null || declare -n $i=delim; done
+  if [ -n "$item1" ] && [ -n "$item2" ]; then
+    combo=( '${c}${d3}$b' '${b}${d3}$c' )
+    stub='${b}${d2}$c'
+  else stub='${b}$c'; fi
 
-  for b in $item1 $item2 ''; do
-    for c in $item1 $item2 ''; do
-        [ -n "$b" -a \( "$c" = "$b" -o -z "$c" \) ] && continue
+  for b in "$item1" "$item2" ''; do
+    for c in "$item2" "$item1" ''; do
+      # short-circuit blank or overlap
+      if [ -n "${b}$c" ]; then [ "$b" != "$c" ] || continue
+      else unset stub; fi
 
-        # create combined suffix 'e' when b and c are empty
-        [ -z "$b$c" -a -n "$item2" -a -n "$item1"  ] &&
-            combo=( "$item2\${d3}$item1" "$item1\${d3}$item2" )
+      for d1 in "${D1[@]}"; do
+        for d2 in "${D2[@]}"; do
+#          if [ -n "$b" ] && [ -n "$c" ]; then stub=${b}${d2}$c ; else stub=${b}$c ; fi
 
-        for d1 in "${D1[@]}"; do
-            for d2 in "${D2[@]}"; do
-                [ -n "$b" -a -n "$c" ] && stub="$b$d2$c" || stub="$b$c"
+          for e in "${combo[@]}"; do
+            [ -n "$e" ] || continue     # d3 is pointless
+            # detect overlap in context of either blank
+#            if [ "$e" = "$b" ] || [ "$e" = "$c" ]; then continue; fi
 
-                for e in "${combo[@]}" "$item2" "$item1"; do
-                    [ -n "$e" ] || continue
-                    [ "$e" = "$b" -o "$e" = "$c" ] && continue
+            for d3 in "${D3[@]}"; do
+              # force '.../config*' pattern
+              [ "$d3" != '/' ] || { log.notice "unsupported final delimiter ($d3)"; continue; }
 
-                    for d3 in "${D3[@]}"; do
-                        # force '.../config*' style, prevent further '/'
-                        [ "$d3" = '/' ] && continue
-
-                        # 'eval' unrolls embedded $d3 inside $e when $combo
-                        eval printf '%s\\n' "${prefix}${stub:+${d1}${stub}}/{.ssh/,}${file}${d3}$e"
-                    done
-                done
-                # without 'e'
-                eval printf '%s\\n' "$prefix${stub:+${d1}${stub}}/{.ssh/,}$file"
-
-                [ -z "$b$c" ] && break 2
-                [ -z "$b" ] && break
+              # 'eval' unrolls embedded
+              eval printf '%s\\n' "${prefix}${stub:+${d1}${stub}}"/{.ssh/,}"${file}${e:+${d3}$e}"
             done
+          done
+          # without 'e'
+          eval printf '%s\\n' "${prefix}${stub:+${d1}${stub}}"/{.ssh/,}"${file}"
+
+          [ -n "$stub" ] || return
+          [ -n "$b" ] || break          # d2 is exhausted
         done
+      done
     done
   done
 }
@@ -104,17 +106,17 @@ function _ssh() {
         for dir in "${SEARCH_DIRS[@]}"; do
           [ -n "$dir" ] || continue
 
-          prefix="$dir" file=config genlist $REGION $PROFILE
+          prefix="$dir" file=config genlist "$REGION" "$PROFILE"
         done
       )
 
-    : ${SSH_CONFIG:?not found}
+    : "${SSH_CONFIG:?not found}"
   fi
 
   # UserKnownHostFile shouldn't be defined inside 'config' because brittle
   #TODO pre-process CONFIG for said directive
 
-  : ${SSH_KNOWN_HOSTS:=${SSH_CONFIG/config/known_hosts}}
+  : "${SSH_KNOWN_HOSTS:=${SSH_CONFIG/config/known_hosts}}"
 
   [ "${SSH_CONFIG%/*}" = "${SSH_KNOWN_HOSTS%/*}" ] ||
       log.warn "mismatched parent" "$SSH_CONFIG" "$SSH_KNOWN_HOSTS"
@@ -133,47 +135,44 @@ function _ssh() {
   case "$1" in
     scp|sftp) cmd=${1^^}
             ;&
-    ssh)    # disable Screen where persistent command output is helpful
-            unset _screen; shift
-            ;;
+    # disable Screen where persistent command output is helpful
+    ssh)    unset _screen; shift ;;
 
     edit)   cmd=${EDITOR:-vi}
             ;&
     # assumes operation against either of SSH_CONFIG or SSH_KNOWN_HOSTS
-    echo|file)
-            declare -n file=${2:-SSH_CONFIG}
-            ${cmd:-$1} "$file"
+    echo|file) [ -f "$2" ] || declare -n file=${2:-SSH_CONFIG}
+            ${cmd:-$1} "${file:-$2}"
             return
   esac
-  : ${cmd:=SSH}
+  : "${cmd:=SSH}"
 
-  # propagate environment when running Screen
+  # save known env since 'screen' discards
   local -a env=(); local -u var
   while read var; do
-    [[ $var =~ SSH_?(IDENTITY|CONFIG|KNOWN_HOSTS|OPTS|VERBOSE) ]] && continue
+    # ignore some SSH_* handled below
+    [[ "$var" =~ SSH_?(IDENTITY|CONFIG|KNOWN_HOSTS|OPTS|VERBOSE) ]] && continue
     [ -n "${!var+X}" ] || continue
 
-    env+=( `printf "%s=%q" "$var" "${!var}"` )
-  done < <( eval printf '%s\\n' DEBUG VERBOSE REGION \${!${CLOUD}_*} ${!SSH_*} )
+    env+=( `printf '%s=%q\n' "$var" "${!var}"` )
+  done < <( eval printf '%s\\n' DEBUG VERBOSE REGION "\${!${CLOUD}_*}" "${!SSH_*}" )
 
   # path-munge if Cygwin|WSL and binary is Windows
   if is_windows "${!cmd}"; then
     for v in SSH_{CONFIG,IDENTITY,KNOWN_HOSTS,LOGFILE}; do
       declare -n nv=$v
-      nv=`convert_path "${!v}"`
+      nv=`convert_path -q "${!v}"`      # silently ignore empty value
     done
   fi
 
   # Add keys to agent
   ssh-add "${SSH_CONFIG%/*}"/*.pem 2>/dev/null
 
-  ${DEBUG:+ runv} "${_screen[@]}" \
-      /bin/env "${env[@]}" "${!cmd}" \
-      ${SSH_VERBOSE:-'-q'} \
-      ${SSH_IDENTITY:+ -i "$SSH_IDENTITY"} \
-      ${SSH_CONFIG:+ -F "$SSH_CONFIG"} \
-      ${SSH_KNOWN_HOSTS:+ -o UserKnownHostsFile="$SSH_KNOWN_HOSTS"} \
-      ${SSH_LOGFILE:+ -E "$SSH_LOGFILE"} \
+  ${DEBUG:+ runv} "${_screen[@]}" /bin/env "${env[@]}" "${!cmd}" ${SSH_VERBOSE:-'-q'} \
+      ${SSH_IDENTITY:+'-i' "$SSH_IDENTITY"} \
+      ${SSH_CONFIG:+'-F' "$SSH_CONFIG"} \
+      ${SSH_KNOWN_HOSTS:+'-o' UserKnownHostsFile="$SSH_KNOWN_HOSTS"} \
+      ${SSH_LOGFILE:+'-E' "$SSH_LOGFILE"} \
       "${SSH_OPTS[@]}" "$@"
 
   # Remove keys (requires .pub files)
@@ -182,28 +181,25 @@ function _ssh() {
 
 
 function init_logs() {
-  local -i _level=0
+  local -i level=0
 
-  [ -n "$VERBOSE" ] && _level=1
-  [ -n "$DEBUG" ] && : $(( _level++ ))
+  [ -n "$VERBOSE" ] && level=1
+  [ -n "$DEBUG" ] && : $((level++))
 
-  [ $_level -ge 1 ] && SSH_VERBOSE="-`printf '%.0sv' {1..$_level}`"
+  [ $level -ge 1 ] && SSH_VERBOSE="-`eval printf '%.0sv' "{1..$level}"`"
 }
 
 
 #--- main ---
 
 _prog="${BASH_SOURCE##*/}"
-_progdir=$( cd `dirname "$BASH_SOURCE"`; pwd )
-_PROG=`readlink -e "$BASH_SOURCE"` || {
-    # impossible error
-    log.error "broken link ($BASH_SOURCE)"
-}
+_progdir=$( cd "`dirname "$BASH_SOURCE"`" && pwd )
+_PROG=`readlink -e "$BASH_SOURCE"` || log.error "broken link ($BASH_SOURCE)"
 _PROGDIR="${_PROG%/*}"
 _PROG="${_PROG##*/}"
-[ "${_prog%.*}" = "${_PROG%.*}" ] || PROFILE="$_prog"
-
 BASEDIR="${_progdir%/bin}"
+
+[ "${_prog%.*}" = "${_PROG%.*}" ] || PROFILE=$_prog
 
 
 declare -i OPTIND=
@@ -213,13 +209,16 @@ while getopts ':qE:F:i:' opt; do
   case "$opt" in
     q)  unset DEBUG VERBOSE ;;
 
+    # trap filename-based args for platform-specific rewrite
     E)  SSH_LOGFILE=$OPTARG ;;
     F)  SSH_CONFIG=$OPTARG ;;
     i)  SSH_IDENTITY=$OPTARG ;;
 
-    \?) # unrecognized, possible long-option
-        case "${@:$OPTIND:1}" in
-#          -var)
+    # possible long-option
+    \?) OPTARG=${!OPTIND}       #alt: ${@:$OPTIND:1}
+        case "$OPTARG" in
+          '--')   break ;;      # impossible?
+#          '-var')
 #                SSH_OPTS+=('-var' "${@:$((++OPTIND)):1}")
 #                ;;
 #          -var-file=*)
@@ -228,20 +227,20 @@ while getopts ':qE:F:i:' opt; do
 
           # assume program option, no arg
           -[46AaCfGgKkMNnqsTtVvXxYy]*)
-                SSH_OPTS+=( "${@:$((OPTIND++)):1}" )
+                SSH_OPTS+=( "$OPTARG" )
                 ;;
           # assume program option, single arg
-          -*)   SSH_OPTS+=( "${@:$((OPTIND++)):1}" )
+          '--'[a-z]*) SSH_OPTS+=( "$OPTARG" )
                 # check next word for option flag, but ignore missing
-                [[ "${@:$OPTIND:1}" =~ ^\- ]] || {
-                    [ -n "${@:$OPTIND:1}" ] && SSH_OPTS+=( "${@:$((OPTIND++)):1}" )
+                declare -i next=$((OPTIND + 1))
+                [[ "${!next}" =~ ^\- ]] || {
+                    [ -n "${!next}" ] && { SSH_OPTS+=( "${!next}" ); : $((OPTIND++)); }
                   }
                 ;;
-          --)   break ;;            # pedant
         esac
+        : $((OPTIND++))
         ;;
     :)  RC=2 log.error "missing argument (-$OPTARG)" ;;
-#    *)  RC=2 log.warn "unhandled option (-$opt)"
   esac
 done
 shift $((OPTIND-1))
@@ -258,7 +257,7 @@ for p in SSH SCP SFTP SCREEN; do
   [ -n "${!p+X}" ] && continue
 
   declare -n pp=$p
-  pp=`VERBOSE=1 is_exec "${p,,}"` || log.warn "missing binary ($p)"
+  pp=`VERBOSE=1 is_exec "${p,,}"`
   log.info "$p == ${!p}"
 done
 
@@ -293,24 +292,23 @@ fi
 
 SEARCH_DIRS=( "$CLOUD_PREFIX" "$BASEDIR" "$HOME/.ssh" )
 
-#TODO $# = 0 and CLOUD; select host from 
+#TODO $# = 0 and CLOUD; select host from
 #   aws ec2 describe-instances --query "Reservations[].Instances[?State.Name == 'running'].InstanceId[]" --output text
 
 # My personal definitions for D{1..3} delimiter sets.
 # One can also override via args to genlist() or change the defaults
 D1='/'
 D2=( '/' '.' )
-
 _ssh "$@"
 
 
-#TODO VerifyHostKeyDNS=yes to query 'IN SSHFP' record, 
+#TODO VerifyHostKeyDNS=yes to query 'IN SSHFP' record,
 # check/add fingerprint: ssh-keyscan <hostname> | ssh-keygen [-E md5] -lf -
 # scrape SSM parameter store for same
 #ref: https://github.com/kepi/ssh-fingerprints
 # VisualHostKey yes for pics, ssh-keygen -lv <pub_key_file> for visual art
 # from just fingerprint requires 3rd party
-#ref: 
+#ref:
 #  https://github.com/atoponce/keyart
 #  https://github.com/openssh/openssh-portable/blob/f703757234a5c585553e72bba279b255a272750a/sshkey.c#L1005-L1100
 #  https://dev.to/krofdrakula/improving-security-by-drawing-identicons-for-ssh-keys-24mc
